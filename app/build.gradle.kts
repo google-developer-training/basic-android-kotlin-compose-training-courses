35c32cdf2dae572ae31b8959bf74f68e029dd79c
Jose Alcérreca <JoseAlcerreca@users.noreply.github.com>
2024-12-09 17:21:07 +0100

    Sdk 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,12 +20,12 @@ plugins {
 
 android {
     namespace = "com.example.courses"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.courses"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
